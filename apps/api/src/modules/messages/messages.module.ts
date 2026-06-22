2fc311d62c0257e30b82a07c7569d082f4a91608
Danielchen7253 <129799498+Danielchen7253@users.noreply.github.com>
2026-06-21 22:01:30 -0500

    Fix messages realtime dependency

--- apps/api/src/modules/messages/messages.module.ts
+++ apps/api/src/modules/messages/messages.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
+import { RealtimeModule } from "../realtime/realtime.module";
 import { MessagesController } from "./messages.controller";
 
-@Module({ controllers: [MessagesController] })
+@Module({ imports: [RealtimeModule], controllers: [MessagesController] })
 export class MessagesModule {}
