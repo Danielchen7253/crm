010c2aa7baa7a991435dc07f70ad5728db013684
Danielchen7253 <129799498+Danielchen7253@users.noreply.github.com>
2026-06-25 23:55:29 -0500

    fix messages module startup by importing calls module

--- apps/api/src/modules/messages/messages.module.ts
+++ apps/api/src/modules/messages/messages.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
+import { CallsModule } from "../calls/calls.module";
 import { RealtimeModule } from "../realtime/realtime.module";
 import { ChannelSenderService } from "./channel-sender.service";
 import { MessagesController } from "./messages.controller";
 
-@Module({ imports: [RealtimeModule], controllers: [MessagesController], providers: [ChannelSenderService] })
+@Module({ imports: [RealtimeModule, CallsModule], controllers: [MessagesController], providers: [ChannelSenderService] })
 export class MessagesModule {}
